c783ff3d19abe97ee39a2f199e2be96778593cbe
sagargajera <sagar.gajera003@gmail.com>
2022-03-23 13:23:09 +0100

    Updated SearchResultPage, Added api configuration, Added searchbar component

--- src/pages/SearchResultsPage.js
+++ src/pages/SearchResultsPage.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import { Text, StyleSheet, View } from 'react-native';
+import SearchBar from '../components/SearchBar';
+import omdb from '../api/omdb';
 
 const SearchResultsPage = () => {
+  const [term, setTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const searchApi = async searchTerm => {
+    try {
+      const response = await omdb.get('/', {
+        params: {
+          apiKey: 'ab645547',
+          s: searchTerm 
+        }
+      });
+      setResults(response.data.Search);
+    } catch (err) {
+      setErrorMessage('Something went wrong');
+    }
+  };
+
+  useEffect(() => {
+    searchApi('sagar');
+  }, []);
+
   return (
-    <View>
-      <Text>Search Results Page</Text>
-    </View>
+    <>
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <View><Text>Movie list goes here</Text></View>
+    </>
   );
 };
 
